324f5341bf2b40b38ccde1fd3adca255494562d2
aharonha <aharon@marketspulse.com>
2018-06-12 17:39:51 +0300

    first commit

---
+++ src/main/java/name/hacah/App.java
@@ -0,0 +1,13 @@
+package name.hacah;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
